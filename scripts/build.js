d9e3db310f8d92bd5fc02838afbcf82bd5897702
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-10-31 13:47:45 +0800

    fix: Update entries

--- scripts/build.js
+++ scripts/build.js
@@ -8,23 +8,46 @@ const dynamicRequiredDirs = ['views']
 const staticFileDirs = ['public']
 
 // Remove old output
-if(fs.existsSync('.zeabur/output')) {
+if (fs.existsSync('.zeabur/output')) {
     console.info('Removing old .zeabur/output')
-    fs.rmSync('.zeabur/output', { recursive: true })
+    fs.rmSync('.zeabur/output', {recursive: true})
+}
+
+function getModuleEntries() {
+    function getModuleEntriesRecursive(dir) {
+        let entries = []
+        fs.readdirSync(dir).forEach(file => {
+            const path = `${dir}/${file}`
+            if (fs.statSync(path).isDirectory()) {
+                if(file === 'node_modules') return
+                entries = entries.concat(getModuleEntriesRecursive(path))
+            } else if (file.endsWith('.js')) {
+                entries.push(path)
+            }
+        })
+        return entries
+    }
+    return getModuleEntriesRecursive('.')
 }
 
 // build with esbuild
 try {
     esbuild.build({
-        entryPoints: ['app.js'],
-        bundle: true,
-        minify: true,
-        sourcemap: true,
-        outfile: '.zeabur/output/functions/index.func/index.js',
+        entryPoints: getModuleEntries(),
+        bundle: false,
+        minify: false,
+        outdir: '.zeabur/output/functions/index.func',
         platform: 'node',
         target: 'node20',
+        plugins: [{
+            name: 'make-all-packages-external',
+            setup(build) {
+                let filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/ // Must not start with "/" or "./" or "../"
+                build.onResolve({filter}, args => ({path: args.path, external: true}))
+            },
+        }],
     }).then(res => {
-        if(res.errors.length > 0) {
+        if (res.errors.length > 0) {
             console.error(res.errors)
             process.exit(1)
         }
@@ -34,16 +57,28 @@ try {
     console.error(error)
 }
 
+// copy node_modules into function output directory
+console.info('Copying node_modules into .zeabur/output/functions/index.func/node_modules')
+fs.cpSync('node_modules', '.zeabur/output/functions/index.func/node_modules', {recursive: true})
+
+// copy package.json into function output directory
+console.info('Copying package.json into .zeabur/output/functions/index.func')
+fs.cpSync('package.json', '.zeabur/output/functions/index.func/package.json')
+
 // copy dynamic-required files into function output directory, so they can be required during runtime
-dynamicRequiredDirs.forEach(dir => { copyIfDirExists(dir, `.zeabur/output/functions/index.func/${dir}`) })
+dynamicRequiredDirs.forEach(dir => {
+    copyIfDirExists(dir, `.zeabur/output/functions/index.func/${dir}`)
+})
 
 // copy static files into function output directory, so they can be served by the web server directly
-staticFileDirs.forEach(dir => { copyIfDirExists(dir, `.zeabur/output/static`) })
+staticFileDirs.forEach(dir => {
+    copyIfDirExists(dir, `.zeabur/output/static`)
+})
 
 function copyIfDirExists(src, dest) {
-    if(fs.statSync(src).isDirectory()) {
+    if (fs.statSync(src).isDirectory()) {
         console.info(`Copying ${src} to ${dest}`)
-        fs.cp(src, dest, { recursive: true }, (err) => {
+        fs.cp(src, dest, {recursive: true}, (err) => {
             if (err) throw err;
         });
         return
