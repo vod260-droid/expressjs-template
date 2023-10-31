a77b2cd08c70f890cfa2fea9b149349a219399a8
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-10-31 16:45:16 +0800

    fix: Copy symlinks

--- scripts/build.js
+++ scripts/build.js
@@ -61,7 +61,7 @@ try {
 
 // copy node_modules into function output directory
 console.info('Copying node_modules into .zeabur/output/functions/index.func/node_modules')
-fs.cpSync('node_modules', '.zeabur/output/functions/index.func/node_modules', {recursive: true})
+fs.cpSync('node_modules', '.zeabur/output/functions/index.func/node_modules', {recursive: true, verbatimSymlinks: true})
 
 // copy package.json into function output directory
 console.info('Copying package.json into .zeabur/output/functions/index.func')
