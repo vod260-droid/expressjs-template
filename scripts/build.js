c1eea11b177617f37b249edb41a8b3fee5ba7cf8
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-10-31 14:00:02 +0800

    fix: Rename `app.js` in output to `index.js`

--- scripts/build.js
+++ scripts/build.js
@@ -52,6 +52,8 @@ try {
             process.exit(1)
         }
         console.info('Successfully built app.js into .zeabur/output/functions/index.func')
+        fs.copyFileSync('.zeabur/output/functions/index.func/app.js', '.zeabur/output/functions/index.func/index.js')
+        fs.rmSync('.zeabur/output/functions/index.func/app.js')
     })
 } catch (error) {
     console.error(error)
